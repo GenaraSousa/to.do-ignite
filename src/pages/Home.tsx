15624eb64176350f28376a1b756ac9647962c4b0
GenaraSousa <genara7sousa@gmail.com>
2021-12-09 00:00:53 -0300

    Desafio finalizado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,37 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+      console.log(newTaskTitle);
+      
+      const newTask: Task = {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false,
+      }
+      setTasks(oldTasks => [...oldTasks, newTask])
+    
   }
 
   function handleToggleTaskDone(id: number) {
+    const updatedTasks = tasks.map( task => {
+      if(task.id===id){
+        return {
+          id : task.id,
+          title: task.title,
+          done: !task.done,
+        }
+      }
+      return task
+    })
+    setTasks(updatedTasks)
     //TODO - toggle task done if exists
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    setTasks(oldTasks => oldTasks.filter(task => 
+      task.id !== id
+    ))
   }
 
   return (
